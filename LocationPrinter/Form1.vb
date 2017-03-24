7202d67f9683ef347f491c9e80c3d861227829ca
Owen <Floul1@hotmail.co.uk>
2017-03-24 16:31:08 +0000

    sad

--- LocationPrinter/Form1.vb
+++ LocationPrinter/Form1.vb
@@ -85,38 +85,150 @@
         If (PrintingSheet)
         e.Graphics.PageUnit = GraphicsUnit.Millimeter
         e.Graphics.InterpolationMode = Drawing2D.InterpolationMode.NearestNeighbor
-            Dim Row as Integer = 0
+            Dim Row as Integer = 1
             Dim Column As Integer = 0
-            Dim Size18B As New Font("Arial", 36.0!, FontStyle.Bold)
             For each Pair As KeyValuePair(Of string,String) in ListOf27Barcodes
-                Row += 1
-                If row = 4
-                    Row = 0
-                    Column += 1
+                Column += 1
+                Dim TextPoint as New Point
+                Dim BarcodePoint as New Point
+
+                If Column = 4
+                    Column = 1
+                    Row += 1
                 End If
-                
+                Select Case Column
+                    Case 1
+                        BarcodePoint.X = 4
+                        TextPoint.X = 10
+                    Case 2
+                        BarcodePoint.X = 69
+                        TextPoint.X = 76
+                    Case 3
+                        BarcodePoint.X = 135
+                        TextPoint.X = 142
+                End Select
+                Select Case Row
+                    Case 1
+                        BarcodePoint.Y = 27
+                        TextPoint.Y = 15
+                    Case 2
+                        BarcodePoint.Y = 57
+                        TextPoint.Y = 45
+                    Case 3
+                        BarcodePoint.Y = 88
+                        TextPoint.Y = 75
+                    Case 4
+                        BarcodePoint.Y = 116
+                        TextPoint.Y = 103
+                    Case 5
+                        BarcodePoint.Y = 146
+                        TextPoint.Y = 133
+                    Case 6
+                        BarcodePoint.Y = 176
+                        TextPoint.Y = 163
+                    Case 7
+                        BarcodePoint.Y = 205
+                        TextPoint.Y = 192
+                    Case 8
+                        BarcodePoint.Y = 234
+                        TextPoint.Y = 221
+                    Case 9
+                        BarcodePoint.Y = 263
+                        TextPoint.Y = 250
+
+                End Select
+                Console.WriteLine("Barcode Point " +BarcodePoint.ToString)
+                Console.WriteLine("Text Point " + TextPoint.ToString)
+
+                    e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode(Pair.Value.PadLeft(6, "0"), 40, 2.25),New Point(BarcodePoint.X,BarcodePoint.Y))
+                    e.Graphics.DrawString(Pair.Key, New Font("Arial", 36,FontStyle.Bold), Black, New RectangleF(TextPoint.X,TextPoint.Y,50,11), GoodFormat)
 
             Next
-
+                        'e.Graphics.DrawRectangles(Pens.Black, {
+                        '  New Rectangle(2, 10, 63.5, 29.6),
+                        '  New Rectangle(68, 10, 63.5, 29.6),
+                        '  New Rectangle(134, 10, 63.5, 29.6),
+                        '  New Rectangle(2, 40, 63.5, 29.6),
+                        '  New Rectangle(68, 40, 63.5, 29.6),
+                        '  New Rectangle(134, 40, 63.5, 29.6),
+                        '  New Rectangle(2, 70, 63.5, 29.6),
+                        '  New Rectangle(68, 70, 63.5, 29.6),
+                        '  New Rectangle(134, 70, 63.5, 29.6),
+                        '  New Rectangle(2, 100, 63.5, 29.6),
+                        '  New Rectangle(68, 100, 63.5, 29.6),
+                        '  New Rectangle(134, 100, 63.5, 29.6),
+                        '  New Rectangle(2, 130, 63.5, 29.6),
+                        '  New Rectangle(68, 130, 63.5, 29.6),
+                        '  New Rectangle(134, 130, 63.5, 29.6),
+                        '  New Rectangle(2, 160, 63.5, 29.6),
+                        '  New Rectangle(68, 160, 63.5, 29.6),
+                        '  New Rectangle(134, 160, 63.5, 29.6),
+                        '  New Rectangle(2, 190, 63.5, 29.6),
+                        '  New Rectangle(68, 190, 63.5, 29.6),
+                        '  New Rectangle(134, 190, 63.5, 29.6),
+                        '  New Rectangle(2, 220, 63.5, 29.6),
+                        '  New Rectangle(68, 220, 63.5, 29.6),
+                        '  New Rectangle(134, 220, 63.5, 29.6),
+                        '  New Rectangle(2, 250, 63.5, 29.6),
+                        '  New Rectangle(68, 250, 63.5, 29.6),
+                        '  New Rectangle(134, 250, 63.5, 29.6)
+                        '  })
 
         Else
             e.Graphics.PageUnit = GraphicsUnit.Millimeter
             e.Graphics.InterpolationMode = Drawing2D.InterpolationMode.NearestNeighbor
+                        e.Graphics.DrawRectangles(Pens.Black, {
+                          New Rectangle(2, 10, 63.5, 29.6),
+                          New Rectangle(68, 10, 63.5, 29.6),
+                          New Rectangle(134, 10, 63.5, 29.6),
+                          New Rectangle(2, 40, 63.5, 29.6),
+                          New Rectangle(68, 40, 63.5, 29.6),
+                          New Rectangle(134, 40, 63.5, 29.6),
+                          New Rectangle(2, 70, 63.5, 29.6),
+                          New Rectangle(68, 70, 63.5, 29.6),
+                          New Rectangle(134, 70, 63.5, 29.6),
+                          New Rectangle(2, 100, 63.5, 29.6),
+                          New Rectangle(68, 100, 63.5, 29.6),
+                          New Rectangle(134, 100, 63.5, 29.6),
+                          New Rectangle(2, 130, 63.5, 29.6),
+                          New Rectangle(68, 130, 63.5, 29.6),
+                          New Rectangle(134, 130, 63.5, 29.6),
+                          New Rectangle(2, 160, 63.5, 29.6),
+                          New Rectangle(68, 160, 63.5, 29.6),
+                          New Rectangle(134, 160, 63.5, 29.6),
+                          New Rectangle(2, 190, 63.5, 29.6),
+                          New Rectangle(68, 190, 63.5, 29.6),
+                          New Rectangle(134, 190, 63.5, 29.6),
+                          New Rectangle(2, 220, 63.5, 29.6),
+                          New Rectangle(68, 220, 63.5, 29.6),
+                          New Rectangle(134, 220, 63.5, 29.6),
+                          New Rectangle(2, 250, 63.5, 29.6),
+                          New Rectangle(68, 250, 63.5, 29.6),
+                          New Rectangle(134, 250, 63.5, 29.6)
+                          })
+            
 
+            'X Diff = 66 Y diff = 30
+            e.Graphics.DrawString("PAL 99",New Font("Arial", 32,FontStyle.Bold),Black,New RectangleF(10,13,50,11))
+            e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode("qlo261171",35,2),New Point(5,28))
+            e.Graphics.DrawString("PAL 99",New Font("Arial", 32,FontStyle.Bold),Black,New RectangleF(76,43,50,11))
+            e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode("qlo261171",35,2),New Point(71,58))
+            e.Graphics.DrawString("PAL 99",New Font("Arial", 32,FontStyle.Bold),Black,New RectangleF(142,73,50,11))
+            e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode("qlo261171",35,2),New Point(137,88))
             'Dim textlocation As RectangleF = New RectangleF(0, 0, 57, 19)
             'Dim subtextlocation As RectangleF = New RectangleF(0, 16, 57, 6)
             'Dim Size18B As New Font("Arial", 36.0!, FontStyle.Bold)
-            Dim Size18B As New Font("Arial", 36.0!, FontStyle.Bold) '36 for small text, 48 for large text
+            'Dim Size18B As New Font("Arial", 36.0!, FontStyle.Bold) '36 for small text, 48 for large text
 
-            e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode("qlo" + CurrentID.ToString.PadLeft(6, "0"), 70, 3), New Point(0, 30)) 'Was 13,19
-            e.Graphics.DrawString(CurrentLabel, Size18B, Black, textlocation, GoodFormat)
+            'e.Graphics.DrawImageUnscaled(barcodes.CreateBarcode("qlo" + CurrentID.ToString.PadLeft(6, "0"), 70, 3), New Point(0, 30)) 'Was 13,19
+            'e.Graphics.DrawString(CurrentLabel, Size18B, Black, textlocation, GoodFormat)
 
-            ''Subtext Black on white
-            'e.Graphics.DrawString(CurrentID + "   " + CurrentType.ToString, Size11, Black, subtextlocation, GoodFormat)
+            'Subtext Black on white
+            ''e.Graphics.DrawString(CurrentID + "   " + CurrentType.ToString, Size11, Black, subtextlocation, GoodFormat)
 
-            'Subtext White on Black
-            e.Graphics.FillRectangle(Brushes.Black, subtextlocation)
-            e.Graphics.DrawString(CurrentID + "   " + CurrentType.ToString, Size11B, White, subtextlocation, GoodFormat)
+            ''Subtext White on Black
+            'e.Graphics.FillRectangle(Brushes.Black, subtextlocation)
+            'e.Graphics.DrawString(CurrentID + "   " + CurrentType.ToString, Size11B, White, subtextlocation, GoodFormat)
 
             'Border for reference - Get rid on the real one
             'e.Graphics.DrawRectangle(New Pen(Color.Black), New Rectangle(0, 0, 57, 33))
@@ -180,13 +292,18 @@
     End Sub
     Private NumberOnSheet as Integer = 27
     Private SheetXSize as Integer 
-    Private ListOfShelves as Dictionary(Of String,String)
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    Private ListOfShelves as New Dictionary(Of String,String)
+
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
         Printer.PrintController = (New Printing.StandardPrintController)
 
         StartButton.Enabled = False
+        ListOfShelves.Clear()
         Try
         For Each shelf As CheckBox In ShelfGroup.Controls
             If shelf.Checked Then
@@ -239,7 +356,7 @@
         End Try
         StartButton.Enabled = True
         End Sub
-    Private ListOf27Barcodes as Dictionary(Of String,String)
+    Private ListOf27Barcodes as new Dictionary(Of String,String)
 
     Private Sub CreateSheetOfBarcodes(Shelves As Dictionary(Of String,String))
         Dim CurrentPair as Integer = 0
@@ -248,10 +365,47 @@
                 ListOf27Barcodes.Add(Pair.Key,Pair.Value)
 
             If ListOf27Barcodes.Count = 27
+                PrintingSheet = True
                 Printer.Print()
                 ListOf27Barcodes.Clear()
+                PrintingSheet = False
             End If
 
         Next
     End Sub
+    Private Function CreateBarcodeLocation(Column as Integer,Row As Integer) As Point
+        Dim ReturnPoint as New Point
+
+        If row = 1
+            ReturnPoint.Y = 30
+        Else if row = 2
+            ReturnPoint.Y = 60
+        Else if row = 3    
+            ReturnPoint.Y = 90
+        Else if row = 4
+            ReturnPoint.Y = 120
+        Else if row = 5
+            ReturnPoint.Y = 150
+        Else if row = 6
+            ReturnPoint.Y = 180
+        Else if row = 7
+            ReturnPoint.Y = 210
+        Else if row = 8
+            ReturnPoint.Y = 240
+        Else if row = 9
+            ReturnPoint.Y = 270
+
+        End If
+        If Column = 1
+            ReturnPoint.X = 6
+        Else if row = 2
+            ReturnPoint.X = 60
+        Else if row = 3    
+            ReturnPoint.X = 100
+
+        End If
+
+
+        Return ReturnPoint
+    End Function
 End Class
